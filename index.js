98231738ad0a0ccdceb202f1e4cb0e906260ed7c
asifikbal01890 <asifikbal01890@gmail.com>
2025-04-17 17:33:50 +0600

    a

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 app.use(cors())
 app.use(express.json())
@@ -23,29 +23,87 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
 
-    const JobsCollection = client.db("jobApply").collection("allJobs");
+    // const JobsCollection = client.db("jobApply").collection("allJobs");
 
-    app.get("/jobs", async(req, res)=>{
-        const result =await JobsCollection.find().toArray()
-        res.send(result)
-    })
+    const db = client.db("jobApply");
+    const allJobsColl = db.collection("allJobs");
+    const applicationsColl = db.collection("applications");
 
-    app.get("/jobs/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await JobsCollection.findOne(query);
-      res.send(result);
-  });
+    //   app.get("/jobs", async(req, res)=>{
+    //       const result =await JobsCollection.find().toArray()
+    //       res.send(result)
+    //   })
 
-  app.post("/jobs", async (req, res) => {
-      const data = req.body;
-      const result = await JobsCollection.insertOne(data);
+    //   app.get("/jobs/:id", async (req, res) => {
+    //     const id = req.params.id;
+    //     const query = { _id: new ObjectId(id) };
+    //     const result = await JobsCollection.findOne(query);
+    //     res.send(result);
+    // });
+
+    // app.post("/jobs", async (req, res) => {
+    //     const data = req.body;
+    //     const result = await JobsCollection.insertOne(data);
+    //     res.send(result);
+    //   });
+
+    app.get("/allJobs", async (req, res) => {
+      const result = await allJobsColl.find().toArray();
       res.send(result);
     });
-  
+
+
+    app.get("/allJobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await allJobsColl.findOne(query);
+      res.send(result);
+    });
+
+    app.get('/myJobs/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const result = await allJobsColl.find(query).toArray()
+      res.send(result)
+    })
+
+    app.post("/allJobs", async (req, res) => {
+      const data = req.body;
+      const result = await allJobsColl.insertOne(data);
+      res.send(result);
+    });
+
+
+
+    app.get("/applications", async (req, res) => {
+      const result = await applicationsColl.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/applications", async (req, res) => {
+      try {
+        const application = {
+          ...req.body,
+          submitted_At: new Date(),
+          status: "pending"
+        };
+        const result = await applicationsColl.insertOne(application);
+        res.status(201).json({ message: "Application submitted", id: result.insertedId });
+      } catch (error) {
+        res.status(500).json({ message: "Error submitting application", error });
+      }
+    });
+
+    app.delete("/applications/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await applicationsColl.deleteOne(query);
+      res.send(result);
+    });
+
   } finally {
-   
-    
+
+
   }
 }
 run().catch(console.dir);
